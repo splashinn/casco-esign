c12250f3025bddf8cde81f9671dd3165762576c4
kyle <the.kyle.maune@gmail.com>
2017-08-30 14:36:58 -0700

    Added a component for Proposals

---
+++ src/components/Proposal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <router-link to="/">Back to Home</router-link>
+    <h2>{{ msg }}</h2>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Listing Signed Proposals'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
